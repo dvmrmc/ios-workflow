9a836e531d1d33f3c0f4c4ab96b6bfd0e5dc31f0
David Martin <david@martinmacias.com>
2016-06-28 19:05:02 +0200

    test

---
+++ library/library/library.h
@@ -0,0 +1,16 @@
+//
+//  library.h
+//  library
+//
+//  Created by David Martin on 6/28/16.
+//  Copyright © 2016 David Martin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface library : NSObject
+
++ (BOOL)isTrue;
++ (BOOL)isFalse;
+
+@end
